2a0579e32c54b8e90d1f142e208a71266548e0c1
Albert <Alb12010@mail.ru>
2023-02-10 02:17:10 +0300

    add Dialogs componenta

--- src/App.js
+++ src/App.js
@@ -2,13 +2,17 @@ import './App.css';
 import Header from "./components/Header/Header";
 import Content from "./components/Content/Content";
 import Sidebar from "./components/Sidebar/Sidebar";
+import Dialogs from "./components/Dialogs/Dialogs";
 
 function App() {
     return (
         <div className="app-wrapper">
             <Header/>
             <Sidebar/>
-            <Content/>
+            <div className="app-wrapper-content">
+                <Dialogs />
+                {/*<Content/>*/}
+            </div>
         </div>
     );
 }

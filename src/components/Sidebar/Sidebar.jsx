9f77931b66451432c69d121d0cd10379954d4353
Albert <Alb12010@mail.ru>
2023-02-10 18:14:34 +0300

    add NavLink, add Active class

--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -1,19 +1,20 @@
 import '../../App.css';
 import classes from "./Sidebar.module.css";
+import {NavLink} from "react-router-dom";
 
 function Sidebar() {
     return (
         <nav className={classes.sidebar}>
             <ul>
-                <li><a href="/profile">Профиль</a></li>
-                <li><a href="/dialogs">Сообщения</a></li>
-                <li><a href="/feed">Новости</a></li>
-                <li><a href="/music">Музыка</a></li>
-                <li><a href="/settings">Настройки</a></li>
+                <li><NavLink to="/profile" className={navData => navData.isActive ? classes.active : classes.colorWhite}>Профиль</NavLink></li>
+                <li><NavLink to="/dialogs" className={navData => navData.isActive ? classes.active : classes.colorWhite}>Сообщения</NavLink></li>
+                <li><NavLink to="/feed" className={navData => navData.isActive ? classes.active : classes.colorWhite}>Новости</NavLink></li>
+                <li><NavLink to="/music" className={navData => navData.isActive ? classes.active : classes.colorWhite}>Музыка</NavLink></li>
+                <li><NavLink to="/settings" className={navData => navData.isActive ? classes.active : classes.colorWhite}>Настройки</NavLink></li>
             </ul>
         </nav>
     );
 }
 
 
-export default  Sidebar;
+export default Sidebar;
